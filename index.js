854a702edbb944cc8620d82e9bb7e8f73e36dd8a
Tomasz Niezgoda <tomek.niezgoda@mail.com>
2015-08-27 11:53:06 +0200

    added browser support

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
+var isNode = require('detect-node');
+
 function determineConfiguration(configurationEnvironmentKey){
   var configurationFilePath;
   var configuration;
 
-  if(!(configurationEnvironmentKey in process.env)){
-    return null;
-  }else{
-    configurationFilePath = process.env[configurationEnvironmentKey];
-    configuration = require(configurationFilePath);
+  if (isNode) {
 
-    return configuration;
+    if(!(configurationEnvironmentKey in process.env)){
+      return null;
+    }else{
+      configurationFilePath = process.env[configurationEnvironmentKey];
+      configuration = require(configurationFilePath);
+
+      return configuration;
+    }
+  }else{
+    
+    if(!(configurationEnvironmentKey in window)){
+      return null;
+    }else{
+      return window[configurationEnvironmentKey]
+    }
   }
 }
 
